b31c4eac72f79e4a9763686cec3d086114a226e1
davidstl <davidstl@bitheads.com>
2018-12-05 12:35:43 -0500

    prep for 3.10.0

--- bcchat/src/globals.h
+++ bcchat/src/globals.h
@@ -27,7 +27,7 @@
 #include "ids.h"
 
 // Application's version
-#define VERSION "0.2.1"
+#define VERSION "3.10.0"
 
 // Max message count to be fetched and displayed per channel
 #define MAX_HISTORY 100
