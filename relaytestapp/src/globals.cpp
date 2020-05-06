60b7bf739965ed07ae6979ec07afa2bc3ddb6726
Daivuk <daivuk@gmail.com>
2020-05-06 07:36:06 -0400

    Added Relay Test App

---
+++ relaytestapp/src/globals.cpp
@@ -0,0 +1,123 @@
+//-----------------------------------------------------------------------------
+// Copyright 2018 bitHeads inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------------
+// File: globals.cpp
+// Desc: Definition for global application state, data and constants
+// Author: David St-Louis
+//-----------------------------------------------------------------------------
+
+// App includes
+#include "globals.h"
+
+// OpenGL
+#if defined(_WIN32) && !defined(APIENTRY)
+#define APIENTRY __stdcall                  // It is customary to use APIENTRY for OpenGL function pointer declarations on all platforms.  Additionally, the Windows OpenGL header needs APIENTRY.
+#endif
+#if defined(_WIN32) && !defined(WINGDIAPI)
+#define WINGDIAPI __declspec(dllimport)     // Some Windows OpenGL headers need this
+#endif
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+// Stb image
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+// Main application state instance
+State state;
+
+// Credentials
+char username[MAX_CREDENTIAL_CHAR] = { '\0' };
+char password[MAX_CREDENTIAL_CHAR] = { '\0' };
+int colorIndex = 0;
+
+// Main window's dimensions
+int width = 1280;
+int height = 720;
+
+// Arrow textures
+ImTextureID ARROWS[8];
+
+// Load configuration file from disk (./config.txt)
+void loadConfigs()
+{
+    char key[256];
+    char value[256];
+
+    auto pFile = fopen("configs.txt", "r");
+    if (pFile)
+    {
+        while (fscanf(pFile, "%s = %s\n", key, value) == 2)
+        {
+            if (strcmp(key, "username") == 0)
+            {
+                strcpy(username, value);
+            }
+            else if (strcmp(key, "password") == 0)
+            {
+                strcpy(password, value);
+            }
+            else if (strcmp(key, "colorIndex") == 0)
+            {
+                colorIndex = std::stoi(value);
+            }
+        }
+        fclose(pFile);
+    }
+
+    // Load arrow textures here too
+    for (int i = 0; i < 8; ++i)
+    {
+        auto filename = "assets/arrow" + std::to_string(i) + ".png";
+        int w, h, bpp;
+        auto pixels = stbi_load(filename.c_str(), &w, &h, &bpp, 4);
+
+        // Upload texture to graphics system
+        GLuint texture;
+        GLint last_texture;
+        glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+        // Cleanup
+        stbi_image_free(pixels);
+
+        // Store our identifier
+        ARROWS[i] = (ImTextureID)(intptr_t)texture;
+
+        // Restore state
+        glBindTexture(GL_TEXTURE_2D, last_texture);
+    }
+}
+
+// Save configuration file to disk (./config.txt)
+void saveConfigs()
+{
+    auto pFile = fopen("configs.txt", "w");
+    if (pFile)
+    {
+        fprintf(pFile, "username = %s\n", username);
+        fprintf(pFile, "password = %s\n", password);
+        fprintf(pFile, "colorIndex = %i\n", colorIndex);
+        fclose(pFile);
+    }
+}
---
+++ relaytestapp/src/globals.h
@@ -0,0 +1,134 @@
+//-----------------------------------------------------------------------------
+// Copyright 2018 bitHeads inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------------
+// File: globals.h
+// Desc: Defines global application state, data and constants
+// Author: David St-Louis
+//-----------------------------------------------------------------------------
+
+// Imgui
+#include <imgui/imgui.h>
+
+// C/C++ includes
+#include <chrono>
+#include <string>
+#include <vector>
+#include <memory>
+
+// brainCloud App settings. Create ids.h and define BRAINCLOUD_SERVER_URL, BRAINCLOUD_APP_ID and BRAINCLOUD_APP_SECRET
+#include "ids.h"
+
+// Application's version
+#define VERSION "4.5.0"
+
+// Max character count that can be used for username and password
+#define MAX_CREDENTIAL_CHAR 32
+
+// Color choices for the game
+static const ImVec4 COLORS[] = {
+    ImGui::ColorConvertU32ToFloat4(0xFF000000),
+    ImGui::ColorConvertU32ToFloat4(0xFF5f4155),
+    ImGui::ColorConvertU32ToFloat4(0xFF646964),
+    ImGui::ColorConvertU32ToFloat4(0xFF5573d7),
+    ImGui::ColorConvertU32ToFloat4(0xFFd78c50),
+    ImGui::ColorConvertU32ToFloat4(0xFF64b964),
+    ImGui::ColorConvertU32ToFloat4(0xFF6ec8e6),
+    ImGui::ColorConvertU32ToFloat4(0xFFfff5dc)
+};
+
+// Screen state enum.
+enum class ScreenState : int
+{
+    Login,          /* Login screen */
+    LoggingIn,
+    MainMenu,       /* Main menu */
+    JoiningLobby,
+    Lobby,          /* Lobby screen */
+    Starting,
+    Game            /* Game screen */
+};
+
+// A point in 2D space
+struct Point
+{
+    int x, y;
+};
+
+// A brainCloud user
+struct User
+{
+    std::string id;         /* Profile Id */
+    std::string name;       /* User name */
+    int colorIndex = 7;
+    bool isReady = false;
+    bool isAlive = false;
+    Point pos = {0, 0};
+};
+
+// Lobby
+struct Lobby
+{
+    std::string lobbyId;
+    std::string ownerId;
+    std::vector<User> members;
+};
+
+// Server info
+struct Server
+{
+    std::string host;
+    int wsPort = -1;
+    int tcpPort = -1;
+    int udpPort = -1;
+    std::string passcode;
+    std::string lobbyId;
+};
+
+// Shockwaves user created with the mouse
+struct Shockwave
+{
+    Point pos;
+    int colorIndex;
+    std::chrono::high_resolution_clock::time_point startTime;
+};
+
+// Main application state. This contain all of the "live" data.
+struct State
+{
+    ScreenState screenState = ScreenState::Login;   /* Current screen we are on */
+    User user;                                      /* Our user */
+    Lobby lobby;                                    /* Lobby with its members as received from brainCloud Lobby Service */
+    Server server;                                  /* Server info (IP, port, protocol, passcode) */
+    std::vector<Shockwave> shockwaves;              /* Players' created shockwaves */
+};
+
+// Load/Save configuration file from/to disk (./config.txt)
+void loadConfigs();
+void saveConfigs();
+
+// Main application state instance
+extern State state;
+
+// Credentials
+extern char username[MAX_CREDENTIAL_CHAR];
+extern char password[MAX_CREDENTIAL_CHAR];
+extern int colorIndex;
+
+// Main window's dimensions
+extern int width;
+extern int height;
+
+// Arrow textures
+extern ImTextureID ARROWS[8];
