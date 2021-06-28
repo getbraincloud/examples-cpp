13ff3146ad09fc6e33e7d4f0b5f9d0525f20592f
Daivuk <daivuk@gmail.com>
2021-06-28 09:25:54 -0400

    Updated version to 4.8

--- relaytestapp/src/globals.h
+++ relaytestapp/src/globals.h
@@ -34,7 +34,7 @@
 #include "ids.h"
 
 // Application's version
-#define VERSION "4.7.0"
+#define VERSION "4.8.0"
 
 // Max character count that can be used for username and password
 #define MAX_CREDENTIAL_CHAR 32
