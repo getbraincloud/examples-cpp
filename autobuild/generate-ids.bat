c727314317c2f80ca21823416f7172917d7cd1c8
jo-codegirl <joanneh@bitheads.com>
2022-08-23 15:07:12 -0600

    windows id script

---
+++ autobuild/generate-ids.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pushd %1
+
+echo #pragma once >ids.h
+
+echo #define BRAINCLOUD_SERVER_URL "%2" >>ids.h
+echo #define BRAINCLOUD_APP_ID "%3" >>ids.h
+echo #define BRAINCLOUD_APP_SECRET "%4" >>ids.h
+
+echo File ids.h created in %1
+type ids.h
+
+popd
