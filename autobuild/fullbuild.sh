b2dea6cf14c855ad3a7bb628b98e2bdf25eccd1e
jo-codegirl <joanneh@bitheads.com>
2022-08-30 06:34:07 -0600

    added utility scripts for windows

--- autobuild/fullbuild.sh
+++ autobuild/fullbuild.sh
@@ -2,6 +2,6 @@ pushd ${2}
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DSSL_ALLOW_SELFSIGNED=1 -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --target ${1} --config Debug
 popd
