916104d3fcbcc0113cf360818598d43047186d07
Joanne Hoar <joanneh@bitheads.com>
2022-09-01 14:44:37 -0600

    updated script

--- autobuild/update-submodules.sh
+++ autobuild/update-submodules.sh
@@ -2,7 +2,7 @@
 if [[ $(git diff --compact-summary) ]];
 then
     echo
-    echo "Warning! Project has outstanding changes. Please commit first."
+    echo "Warning! Project has outstanding changes. Please commit or revert first."
     echo
     exit 1
 fi
