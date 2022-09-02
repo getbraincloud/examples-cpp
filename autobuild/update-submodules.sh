048f975288e53560a97c1070ca32222c85ae47c3
Joanne Hoar <joanneh@bitheads.com>
2022-09-02 10:53:39 -0600

    make sure to use ssh url to push

--- autobuild/update-submodules.sh
+++ autobuild/update-submodules.sh
@@ -11,16 +11,22 @@ for i in thirdparties/braincloud-cpp roomserver/server/brainclouds2s-cpp
 do
     echo
     STR=$(git config -f .gitmodules --get submodule.$i.branch)
-
+    
     if [[ $(git submodule update --remote $i) ]];
     then
+        # make sure we use ssh url to push
+        fullUrl=$(git config -f .gitmodules --get submodule.$i.url)
+        pushd $i
+        git remote set-url origin ${fullUrl/"https://github.com/"/"git@github.com:"}
+        popd
+        
         git add $i
         git commit -m "automatic submodules update" .
         git push
         echo "--- $i updated on branch $STR"
+    fi
     else
         echo "--- $i is already up to date"
     fi
-
 done
 echo
