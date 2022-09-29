4dfe774ecc6877c8633622a90f8abfee3de5adde
Joanne Hoar <joanneh@bitheads.com>
2022-09-29 08:50:10 -0600

    android build script

--- autobuild/launch-android.sh
+++ autobuild/launch-android.sh
@@ -3,23 +3,23 @@
 
 case "$1" in
     devices)
-        adb devices
+        $ANDROID_SDK/platform-tools/adb devices
         ;;
     start)
-        adb -s $2 shell am start -n com.bitheads.braincloud.android.debug/com.bitheads.braincloud.android.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+        $ANDROID_SDK/platform-tools/adb -s $2 shell am start -n com.bitheads.braincloud.android.debug/com.bitheads.braincloud.android.MainActivity -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
         ;;
     stop)
-        adb -s $2 shell am force-stop com.bitheads.braincloud.android.debug
+        $ANDROID_SDK/platform-tools/adb -s $2 shell am force-stop com.bitheads.braincloud.android.debug
         ;;
      
     install)
-        adb -s $2  install -t $3/app/build/outputs/apk/debug/app-debug.apk
+        $ANDROID_SDK/platform-tools/adb -s $2  install -t $3/app/build/outputs/apk/debug/app-debug.apk
         ;;
     uninstall)
-    adb -s $2 uninstall com.bitheads.braincloud.android.debug
+        $ANDROID_SDK/platform-tools/adb -s $2 uninstall com.bitheads.braincloud.android.debug
         ;;
     clear)
-    adb -s $2 shell pm clear com.bitheads.braincloud.android.debug
+        $ANDROID_SDK/platform-tools/adb -s $2 shell pm clear com.bitheads.braincloud.android.debug
         ;;
      
     *)
