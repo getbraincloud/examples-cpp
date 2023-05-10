f8cae6c9f8e348117c34262a986422783d8b8ad9
jo-codegirl <joanneh@bitheads.com>
2023-05-10 06:14:08 -0600

    this is a test

--- relaytestapp/src/ids.h
+++ relaytestapp/src/ids.h
@@ -1,4 +1,4 @@
 #pragma once
 #define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherV2"
-#define BRAINCLOUD_APP_ID ""
-#define BRAINCLOUD_APP_SECRET ""
+#define BRAINCLOUD_APP_ID "23649"
+#define BRAINCLOUD_APP_SECRET "a754a2c0-72d9-46ce-9fdf-18e9c19a556c"
