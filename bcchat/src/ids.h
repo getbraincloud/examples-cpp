ab48a695ac4fb880d491e9d60fd2871fc2839247
jo-codegirl <joanneh@bitheads.com>
2023-05-10 06:06:22 -0600

    checked in files that should be ignored

--- bcchat/src/ids.h
+++ bcchat/src/ids.h
@@ -1,4 +1,4 @@
 #pragma once
-#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherV2"
-#define BRAINCLOUD_APP_ID "22682"
-#define BRAINCLOUD_APP_SECRET "9c0b59dc-f074-4e62-b54f-e891d0cc66d9"
+#define BRAINCLOUD_SERVER_URL ""
+#define BRAINCLOUD_APP_ID ""
+#define BRAINCLOUD_APP_SECRET ""
--- hellobc/ids.h
+++ hellobc/ids.h
@@ -1,4 +1,4 @@
 #pragma once
-#define BRAINCLOUD_SERVER_URL "https://api.internal.braincloudservers.com/dispatcherV2"
-#define BRAINCLOUD_APP_ID "20001"
-#define BRAINCLOUD_APP_SECRET "4e51b45c-030e-4f21-8457-dc53c9a0ed5f"
+#define BRAINCLOUD_SERVER_URL ""
+#define BRAINCLOUD_APP_ID ""
+#define BRAINCLOUD_APP_SECRET ""
