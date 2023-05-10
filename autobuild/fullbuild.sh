55a5eef1dcc2ad18a553ab320a8b55f7fc34b762
jo-codegirl <joanneh@bitheads.com>
2023-05-10 13:06:55 -0600

    using ninja build

--- autobuild/fullbuild.sh
+++ autobuild/fullbuild.sh
@@ -1,7 +1,9 @@
+NINJA_COMMAND=/Volumes/CLion/CLion.app/Contents/bin/ninja/mac/ninja
+
 pushd ${2}
 rm -rf build
 mkdir build
 cd build
-cmake -DBC_USE_OPENSSL=1 -DCMAKE_BUILD_TYPE=Debug ..
+cmake -GNinja -DCMAKE_MAKE_PROGRAM=${NINJA_COMMAND} -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . --target ${1} --config Debug
 popd
