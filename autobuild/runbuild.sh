e62b6312d964498fe57fcc8bd48d922c2823abf3
jo-codegirl <joanneh@bitheads.com>
2023-05-15 10:45:12 -0600

    ninja build script

--- autobuild/runbuild.sh
+++ autobuild/runbuild.sh
@@ -1,8 +1,2 @@
-pushd ${2}
-rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --target ${1} --config Debug
-./${1}
-popd
+./autobuild/fullbuild.sh ${1} ${2:-$1}
+./${2:-$1}/build/${1}
