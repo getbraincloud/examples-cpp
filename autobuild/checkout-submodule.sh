5858dfe673906d0bd4400deb73b05b80f1ce81b6
jo-codegirl <joanneh@bitheads.com>
2023-05-30 14:10:32 -0600

    build script

--- autobuild/checkout-submodule.sh
+++ autobuild/checkout-submodule.sh
@@ -2,12 +2,13 @@
 
 if [[ -d $1 ]];
 then
+		STR=$(git config -f .gitmodules --get submodule.$1.branch)
 	    pushd $1 
 	    if [[ $(git diff --compact-summary .) ]];
     	then
 			echo "Folder has mods"
 		else
-			git checkout $2
+			git checkout ${2:-$STR}
 			git pull
 		fi
 		popd				
