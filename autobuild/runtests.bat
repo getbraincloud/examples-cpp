e8a860828bcd3d1fff25c36fcbb3bc8ccf1c3802
jo-codegirl <joanneh@bitheads.com>
2023-05-31 12:30:00 -0600

    windows build script

--- autobuild/runtests.bat
+++ autobuild/runtests.bat
@@ -2,8 +2,8 @@
 rmdir /S /Q build
 mkdir build
 pushd build
-copy /Y c:\Users\buildmaster\bin\test-ids-internal.txt ids.txt
-cmake -DSSL_ALLOW_SELFSIGNED=ON -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_TESTS=ON ../%1
+copy /Y ..\%1\ids.txt .
+cmake -DSSL_ALLOW_SELFSIGNED=ON -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug ../%1
 cmake --build . --target bctests --config Debug
 tests\Debug\bctests.exe --test_output=all --gtest_output=xml:tests\results.xml --gtest_filter=*TestBC%2*
 popd
