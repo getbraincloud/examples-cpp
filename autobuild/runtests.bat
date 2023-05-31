f73daa373fdcffbec71f2cf2431b5e49420368bd
jo-codegirl <joanneh@bitheads.com>
2023-05-31 06:54:32 -0600

    jenkinsfile windows build script

--- autobuild/runtests.bat
+++ autobuild/runtests.bat
@@ -1,8 +1,8 @@
-@echo off
-copy /Y c:\Users\buildmaster\bin\test-ids-internal.txt ..\%1\ids.txt
+::@echo off
 rmdir /S /Q build
 mkdir build
 pushd build
+copy /Y c:\Users\buildmaster\bin\test-ids-internal.txt ids.txt
 cmake -DSSL_ALLOW_SELFSIGNED=ON -DCMAKE_GENERATOR_PLATFORM=x64 -DBUILD_TESTS=ON ../%1
 cmake --build . --target bctests --config Debug
 tests\Debug\bctests.exe --test_output=all --gtest_output=xml:tests\results.xml --gtest_filter=*TestBC%2%*
